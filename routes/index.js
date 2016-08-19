d4a3a0f5c5880c128834aa4bef7461cf68ec41eb
Pakkawat <hern.suwannasam@gmail.com>
2016-08-20 02:06:48 +0700

    add webicon

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,34 @@ router.get('/blog', function(req, res, next) {
   res.render('blog',{title:getTxtData[6].title,des:getTxtData[6].description,
 						first:getTxtData[6].first,second:getTxtData[6].second});
 });
-
+router.get('/windows', function(req, res, next) {
+  res.render('windows',{title:getTxtData[7].title,des:getTxtData[7].description,
+						first:getTxtData[7].first,second:getTxtData[7].second});
+});
+router.get('/ios', function(req, res, next) {
+  res.render('ios',{title:getTxtData[8].title,des:getTxtData[8].description,
+						first:getTxtData[8].first,second:getTxtData[8].second});
+});
+router.get('/android', function(req, res, next) {
+  res.render('android',{title:getTxtData[9].title,des:getTxtData[9].description,
+						first:getTxtData[9].first,second:getTxtData[9].second});
+});
+router.get('/computer', function(req, res, next) {
+  res.render('computer',{title:getTxtData[10].title,des:getTxtData[10].description,
+						first:getTxtData[10].first,second:getTxtData[10].second});
+});
+router.get('/iphone', function(req, res, next) {
+  res.render('iphone',{title:getTxtData[11].title,des:getTxtData[11].description,
+						first:getTxtData[11].first,second:getTxtData[11].second});
+});
+router.get('/printer', function(req, res, next) {
+  res.render('printer',{title:getTxtData[12].title,des:getTxtData[12].description,
+						first:getTxtData[12].first,second:getTxtData[12].second});
+});
+router.get('/camera', function(req, res, next) {
+  res.render('camera',{title:getTxtData[13].title,des:getTxtData[13].description,
+						first:getTxtData[13].first,second:getTxtData[13].second});
+});
 // });
 router.post('/view1', function(req, res) {
 	res.setHeader('Content-Type', 'text/plain');
